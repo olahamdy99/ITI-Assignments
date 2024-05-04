aea559ae8e967ae7de4984d605f5b728e6187113
Ola Hamdy <152440610+olahamdy99@users.noreply.github.com>
2024-05-04 23:38:16 +0300

    Add files via upload

---
+++ Angular/Lab 2/Users/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HeaderComponent,CardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'lab2';
+}
